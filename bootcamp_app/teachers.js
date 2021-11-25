1118b1c5fafedd1a5509d15d33efeecd13aaae6d
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-25 03:22:29 -0500

    teachers update javascript

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,16 +6,22 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-const comm = `SELECT students.id, students.name AS name, cohorts.name as cohort
-FROM students JOIN cohorts 
+const comm = `SELECT  DISTINCT cohorts.name as cohort,teachers.name as teacher
+FROM teachers
+JOIN assistance_requests
+ON teachers.id = teacher_id
+JOIN students
+ON students.id = student_id
+JOIN cohorts
 ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+ORDER BY teacher
+ LIMIT ${process.argv[3] || 5};
 `
 pool.query(comm)
 .then(result =>{ 
   result.rows.forEach(user =>{
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort}:  ${user.teacher} and was in the cohort`);
   })
  
 }).catch(err => {
